fff826cf5ae49c6447258e7ea4b13816c4693153
Luca Ponteprimo <lucaponteprimo04@gmail.com>
2023-11-07 10:10:30 -0300

    Axios dependencie

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
-
+import {useForm} from 'react-hook-form'
 function RegisterPage() {
+
+    const {register, handleSubmit} = useForm()
+
   return (
-    <div>RegisterPage</div>
+    <section className='bg-zinc-700 max-w-md p-10 rounded-md'>
+   <form onSubmit={handleSubmit((values) => {
+    console.log(values)
+   })}>
+    <input type="text" placeholder='Nombre'{... register("username", {required: true})} className='bg-zinc-500 text-white px-4 rounded-md py-2 my-2 w-full'/>
+    <input type="email" placeholder='E-mail'{... register("email", {required: true})} className='bg-zinc-500 text-white px-4 rounded-md py-2 my-2 w-full'/>
+    <input type="password" placeholder='Contraseña'{... register("password", {required: true})} className='bg-zinc-500 text-white px-4 rounded-md py-2 my-2 w-full'/>
+    <button type='submit' className='bg-slate-400 rounded-lg px-4 py-2 my-2'>Register</button>
+
+   </form>
+   </section>
   )
 }
-
 export default RegisterPage
