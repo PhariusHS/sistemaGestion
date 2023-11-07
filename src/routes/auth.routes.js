16279b24f23fb65659b14a517d3bc39c6dc87e64
Vaabrate <93230295+Vaabrate@users.noreply.github.com>
2023-11-07 11:15:34 -0300

    validación de token con cookies

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
-import {login, logout, register} from "../controllers/auth.controllers.js"
+import {login, logout, register, profile} from "../controllers/auth.controllers.js"
+import { authRequired } from "../middleware/validateToken.js";
 
 const router = Router();
 //Rutas de acceso backend
 router.post('/register', register)
 router.post('/login', login)
 router.post('/logout', logout)
+router.get("/profile", authRequired, profile)
 export default router
