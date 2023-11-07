ae97718ad1bf35fb125d2ddb52bf030b1a620d61
Luca Ponteprimo <lucaponteprimo04@gmail.com>
2023-11-07 08:53:01 -0300

    Agregado tailwindcss

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,16 @@
 import { useState } from 'react';
-import './App.css';
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
 
-    </>
+    <div>
+    <h1 className="text-9xl font-bold underline">
+      Hello world!
+    </h1>
+    </div>
+
   )
 }
 
