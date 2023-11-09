27b11c1ac2dc28b6d34953b29a39ed54d7309322
matumir <136464954+matumir@users.noreply.github.com>
2023-11-09 10:47:28 -0300

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,13 +2,14 @@ import { useState } from 'react';
 import {BrowserRouter, Routes, Route} from "react-router-dom"
 import RegisterPage from './pages/RegisterPage'
 import LoginPage from './pages/LoginPage'
+import Home from './pages/Home'
 function App() {
 
   return (
 
     <BrowserRouter>
           <Routes>
-                <Route path='/' element={<></>}></Route>
+                <Route path='/home' element={<Home></Home>}></Route>
                 <Route path='/login' element={<LoginPage></LoginPage>}></Route>
                 <Route path='/register' element={<RegisterPage></RegisterPage>}></Route>
                 <Route path='/pedidos-listos' element={<></>}></Route>
