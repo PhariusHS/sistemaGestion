373eb64507ea7ab956592dd24eb649d2e7109c93
Vaabrate <93230295+Vaabrate@users.noreply.github.com>
2023-11-14 09:38:19 -0300

    validateSchema

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
-import {login, logout, register, profile} from "../controllers/auth.controllers.js"
+import {login, logout, register, profile} from "../controllers/auth.controllers.js";
 import { authRequired } from "../middleware/validateToken.js";
+import {validateSchema} from '../middleware/validator.middleware.js'
+import {registerSchema, loginSchema} from '../schema/auth.schema.js'
 
 const router = Router();
 //Rutas de acceso backend
-router.post('/register', register)
-router.post('/login', login)
+router.post('/register', validateSchema(registerSchema), register)
+router.post('/login', validateSchema(loginSchema), login)
 router.post('/logout', logout)
 router.get("/profile", authRequired, profile)
 export default router
